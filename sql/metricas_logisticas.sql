f7f5decfc76b545dd2148beeac156cf8723e1c9a
Mateus <146376874+ateteu@users.noreply.github.com>
2026-05-18 09:45:45 -0300

    Faz pequenas alterações de legibilidade

--- sql/metricas_logisticas.sql
+++ sql/metricas_logisticas.sql
@@ -4,40 +4,40 @@ CREATE OR REPLACE TABLE metricas_logisticas AS
 
 -- dist_base_principal_km   : Distância (km) do município em questão até a base principal, em betim
 -- dist_base_oliveira_km    : Distância (km) do município em questão até a base em Oliveira
--- base_atendimento         : Base responsável (Betim ou Oliveira) pelo atendimento do município
+-- base_atendimento         : ID da base responsável (Betim ou Oliveira) pelo atendimento do município
 -- dist_base_atendimento_km : Distância (km) do município em questão até a base responsável pelo atendimento do município
 
 WITH distancias AS (
     SELECT 
-    id_municipio,
+        id_municipio,
 
-    -- Distância Haversine do município selecionado até Betim:
-    (
-        6371 * 2 * ASIN(
-            SQRT(
-                POWER(SIN(RADIANS(latitude - (-19.9668)) / 2), 2)
-                +
-                COS(RADIANS(-19.9668))
-                * COS(RADIANS(latitude))
-                * POWER(SIN(RADIANS(longitude - (-44.2008)) / 2), 2)
+        -- Distância Haversine do município selecionado até Betim:
+        (
+            6371 * 2 * ASIN(
+                SQRT(
+                    POWER(SIN(RADIANS(latitude - (-19.9668)) / 2), 2)
+                    +
+                    COS(RADIANS(-19.9668))
+                    * COS(RADIANS(latitude))
+                    * POWER(SIN(RADIANS(longitude - (-44.2008)) / 2), 2)
+                )
             )
-        )
-    ) AS dist_base_principal_km,
+        ) AS dist_base_principal_km,
 
-    -- Distância Haversine do município selecionado até Oliveira:
-    (
-        6371 * 2 * ASIN(
-            SQRT(
-                POWER(SIN(RADIANS(latitude - (-20.6982)) / 2), 2)
-                +
-                COS(RADIANS(-20.6982))
-                * COS(RADIANS(latitude))
-                * POWER(SIN(RADIANS(longitude - (-44.8290)) / 2), 2)
+        -- Distância Haversine do município selecionado até Oliveira:
+        (
+            6371 * 2 * ASIN(
+                SQRT(
+                    POWER(SIN(RADIANS(latitude - (-20.6982)) / 2), 2)
+                    +
+                    COS(RADIANS(-20.6982))
+                    * COS(RADIANS(latitude))
+                    * POWER(SIN(RADIANS(longitude - (-44.8290)) / 2), 2)
+                )
             )
-        )
-    ) AS dist_base_oliveira_km
+        ) AS dist_base_oliveira_km
 
-    FROM coordenadas_municipios.csv
+    FROM coordenadas_municipios
 )
 
 SELECT 
