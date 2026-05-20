f0510c8e3176a75c2975ec2144d223ad13f4185f
Mateus <146376874+ateteu@users.noreply.github.com>
2026-05-20 10:10:48 -0300

    Finaliza query SQL do score_logistico

--- sql/2-pontuacoes/score_logistico.sql
+++ sql/2-pontuacoes/score_logistico.sql
@@ -1,21 +1,46 @@
 CREATE OR REPLACE TABLE score_logistico AS
 
-WITH referencia AS (
+-- Resumo das métricas:
 
-    SELECT
-        l.id_municipio,
-        l.dist_base_atendimento_km
+-- score_logistico : 
+    -- Score baseado na distância até a base mais próxima;
+    -- valores próximos de 1 indicam atendimento eficiente
 
-    FROM metricas_logisticas l
+WITH 
+    referencia AS (
+        SELECT
+            l.id_municipio,
+            l.dist_base_atendimento_km
 
-    INNER JOIN municipios_atendidos a
-        ON l.id_municipio = a.id_municipio
-)
+        FROM metricas_logisticas l
+
+        INNER JOIN municipios_atendidos a
+            ON l.id_municipio = a.id_municipio
+        
+        WHERE a.status_atendimento = 'atendido'
+    ),
+
+    distancia_referencia AS (
+        -- Calcula a distância de referência município-base
+        -- com base nos municípios atendidos (percentil 90%)
+        SELECT
+            quantile_cont(
+                dist_base_atendimento_km,
+                0.90
+            ) AS distancia_referencia_km
+
+        FROM referencia
+    )
 
 SELECT
-    quantile_cont(
-        dist_base_atendimento_km,
-        0.90
-    ) AS distancia_referencia_km
+    l.*,
+    d.distancia_referencia_km,
 
-FROM referencia
+    (
+        1.0 
+        / 
+        (1.0 + (l.dist_base_atendimento_km  / d.distancia_referencia_km))
+    ) AS score_logistico
+
+FROM metricas_logisticas l
+CROSS JOIN distancia_referencia d
