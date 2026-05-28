e3298260803110db3603c1a27c766f74b7082a3a
Mateus <146376874+ateteu@users.noreply.github.com>
2026-05-27 21:19:43 -0300

    Melhora legibilidade do código

--- sql/1-metricas/anuais/metricas_economicas.sql
+++ sql/1-metricas/anuais/metricas_economicas.sql
@@ -3,19 +3,23 @@ CREATE OR REPLACE TABLE metricas_economicas AS
 -- Resumo das métricas:
 
 -- pib_per_capita_relativo     : Relação entre o PIB per capita municipal e o PIB per capita médio estadual
+-- pib_pc_relativo_norm        : PIB per capita relativo normalizado entre 0 e 1 (min-max normalization)
 -- contribuicao_agro           : Participação (%) da agropecuária no VAB total do município
 -- contribuicao_industria      : Participação (%) da indústria no VAB total do município
+-- contribuicao_industria_norm : Participação (%) da indústria no VAB normalizada entre 0 e 1
 -- contribuicao_servicos       : Participação (%) dos serviços no VAB total do município
-
--- pib_pc_relativo_norm        : PIB per capita relativo normalizado entre 0 e 1 (min-max normalization)
--- contribuicao_industria_norm : Participação da indústria no VAB normalizada entre 0 e 1
--- contribuicao_servicos_norm  : Participação dos serviços no VAB normalizada entre 0 e 1
+-- contribuicao_servicos_norm  : Participação (%) dos serviços no VAB normalizada entre 0 e 1
 
 WITH 
     pib_estadual AS (
         SELECT
             ano,
-            SUM(pib_total) / SUM(populacao) AS pib_per_capita_estadual
+
+            (
+                SUM(pib_total) 
+                / 
+                SUM(populacao)
+            ) AS pib_per_capita_estadual
 
         FROM pib_ibge
         GROUP BY ano
@@ -39,7 +43,9 @@ WITH
             AS contribuicao_servicos
 
         FROM pib_ibge p
-        LEFT JOIN pib_estadual pe ON p.ano = pe.ano
+
+        LEFT JOIN pib_estadual pe 
+            ON p.ano = pe.ano
     )
 
 SELECT
@@ -53,8 +59,7 @@ SELECT
         0
     )
     AS pib_pc_relativo_norm,
-
-
+    ---------------------------------------------------------------------------
     -- Normalização da contribuição da indústria
     (contribuicao_industria - MIN(contribuicao_industria) OVER())
     /
@@ -63,8 +68,7 @@ SELECT
         0
     )
     AS contribuicao_industria_norm,
-
-
+    ---------------------------------------------------------------------------
     -- Normalização da contribuição de serviços
     (contribuicao_servicos - MIN(contribuicao_servicos) OVER())
     /
