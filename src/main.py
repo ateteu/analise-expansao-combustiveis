4606d09d333076849b93d948733245734b7c3488
Mateus <146376874+ateteu@users.noreply.github.com>
2026-05-28 15:22:03 -0300

    Atualiza main para executar todos os processos

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import warnings
-from pipelines.senatran    import executar as executar1
-from pipelines.pib         import executar as executar2
-from pipelines.vendas_anp  import executar as executar3
+from pipelines.senatran    import executar as executar_ppl_senatran
+from pipelines.pib         import executar as executar_ppl_pib
+from pipelines.vendas_anp  import executar as executar_ppl_vendas
+from bd.carregar_duckdb    import carregar_bd
+from bd.executar_sql       import executar_queries_sql
 
 
 # Ignorar avisos sobre headers e footers
@@ -13,18 +15,21 @@ warnings.filterwarnings("ignore", category = UserWarning, module = "openpyxl")
 
 def main():
     """
-    Executa todos os pipelines de tratamento de dados.
+    Executa todos os pipelines de tratamento/limpeza de dados,
+    carrega o BD e executa as queries SQL.
     """
-    pipelines = [
-        ("SENATRAN"   , executar1),
-        ("PIB"        , executar2),
-        #("VENDAS_ANP" , executar3), # Utilizar o outro pipeline, mais robusto
+    passos = [
+        #("SENATRAN"     , executar_ppl_senatran),
+        #("PIB"          , executar_ppl_pib),
+        #("VENDAS ANP"   , executar_ppl_vendas), # Deixar comentado por enquanto
+        ("BANCO DE DADOS", carregar_bd),
+        ("QUERIES SQL"   , executar_queries_sql)
     ]
 
-    # Se um pipeline falhar, tenta o próximo
-    for nome, pipeline in pipelines:
+    # Se um processo falhar, tenta o próximo
+    for nome, passo in passos:
         try:
-            pipeline()
+            passo()
 
         except Exception as erro:
             print(f"\nFalha em {nome}: {erro}")
