4e95772945d65ca0b0ab4d1fe4c49646ce697562
Mateus <146376874+ateteu@users.noreply.github.com>
2026-05-28 13:24:24 -0300

    Atualiza caminhos utilizados pelos arquivos

--- src/configs/caminhos.py
+++ src/configs/caminhos.py
@@ -1,21 +1,20 @@
-# Este arquivo contém caminhos utilizados nos códigos
-
 from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-DADOS_BRUTOS         = BASE_DIR / "dados" / "1-brutos"
-DADOS_MODIFICADOS    = BASE_DIR / "dados" / "2-intermediarios"
+DADOS_BRUTOS      = BASE_DIR / "dados" / "1-brutos"
+DADOS_MODIFICADOS = BASE_DIR / "dados" / "2-intermediarios"
+DADOS_MODELADOS   = BASE_DIR / "dados" / "3-modelados"
 
 
 ARQUIVO_PIB          = DADOS_BRUTOS / "pib-ibge" / "PIB dos Municipios - base de dados 2010-2023.xlsx"
 ARQUIVO_CODIGOS_IBGE = DADOS_BRUTOS / "codigos-ibge" / "RELATORIO_DTB_BRASIL_2024_MUNICIPIOS.xls"
 
 
-CAMINHO_SENATRAN     = DADOS_BRUTOS / "frota-senatran"
-CAMINHO_VENDAS_ANP   = DADOS_BRUTOS / "vendas-anp"
+CAMINHO_SENATRAN   = DADOS_BRUTOS / "frota-senatran"
+CAMINHO_VENDAS_ANP = DADOS_BRUTOS / "vendas-anp"
 
 
 ARQUIVO_BD                     = BASE_DIR / "dados" / "banco_dados.duckdb"
